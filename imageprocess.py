a7582e08fafc1b286f551dbd0f989a9d3ee4f34c
MincoMK <mail@drchi.co.kr>
2023-10-27 19:46:17 +0900

    aa

--- imageprocess.py
+++ imageprocess.py
@@ -6,4 +6,5 @@ for file in files:
     if file.name.endswith(".json"):
         os.system(f"netlistsvg {file.path} -o net{i}.svg")
         os.system(f"convert -background white -alpha remove -alpha off net{i}.svg net{i}.png")
+        print(f"[SYNTHGEN] Generated net{i}.png")
         i += 1
--- synthgen.py
+++ synthgen.py
@@ -6,17 +6,10 @@ for file in files:
     if file.name.endswith(".v"):
         synth = ""
         synth += f"read_verilog -sv {file.path}\n"
-        synth += f"""proc
-opt
-memory
-opt
-techmap
-opt
-splitnets
-opt
-write_json net{i}.json"""
+        synth += f"proc\nopt\nmemory\nopt\ntechmap\nopt\nsplitnets\nwrite_json net{i}.json"
         with open(f"synth{i}.ys", "w") as f:
             f.write(synth)
+        print(f"[SYNTHGEN] Generated synth{i}.ys")
         os.system(f"yosys synth{i}.ys")
         i += 1
 
