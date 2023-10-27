3d63757c1344da1d0857df00e763ecc61421d514
MincoMK <mail@drchi.co.kr>
2023-10-27 19:56:15 +0900

    aa

--- webhook.py
+++ webhook.py
@@ -3,6 +3,7 @@ import requests, os
 WEBHOOK_URL = "https://discord.com/api/webhooks/1167333653504851978/2QW80l5Nf4j3t_G56jF1EzH8nVUY0dDtK4y0pDemiQmXt7Z3-64aW2rwVi1wmPyd6HnY"
 requests.post(WEBHOOK_URL, json={"content": "Synthesis complete!"})
 files = os.scandir('.')
+os.remove("net.png")
 for file in files:
     if file.name.endswith(".png"):
         requests.post(WEBHOOK_URL, files={"net": open(file.path, "rb")})
